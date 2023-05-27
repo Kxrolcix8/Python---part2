406927c5f4f40ad2395359b77e77e6e83101f519
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2023-05-27 19:49:10 +0200

    update

---
+++ Struktury_danych/zadanie2.py
@@ -0,0 +1,8 @@
+slowo = (input("Podaj dowolne słowo i sprawdź czy jest palindromem: ")) 
+  
+o_slowo = slowo[::-1]
+
+if (slowo == o_slowo):
+    print("To palindrom")
+else:
+    print("To nie palindrom")
