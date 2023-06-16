1f41cb1f40d2fa742684324d68525de260d6c58b
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2023-06-16 20:06:20 +0200

    Delete Struktury_danych directory

--- Struktury_danych/zadanie2.py
+++
@@ -1,8 +0,0 @@
-slowo = str(input("Podaj dowolne słowo i sprawdź czy jest palindromem: ")) 
-  
-o_slowo = slowo[::-1]
-
-if (slowo == o_slowo):
-    print("To palindrom")
-else:
-    print("To nie palindrom")
--- Struktury_danych/zadanie3.py
+++
@@ -1,5 +0,0 @@
-l = ["burak","cukinia","pomidor","cytryna" ,"ananas", "paptyka", "dynia"]
-litera = input("Podaj litere: ")
-for element in l:
-    if element.startswith(litera):
-        print(element, end=" ")
