7e56cf50ef74e17b951dbef2b8c46f855aba2599
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2023-06-16 20:24:09 +0200

    Delete ify directory

--- ify/zadanie1.py
+++
@@ -1,7 +0,0 @@
-wiek = int(input("Hej! Podaj swój wiek: "))
-
-if wiek >= 18:
-    print("Jesteś pełnoletni/a!")
-
-else:
-    print("Jesteś niepełnoletni/a!")
--- ify/zadanie2.py
+++
@@ -1,4 +0,0 @@
-for i in range (0,101,1):
-    if i % 3 == 0:
-        print(i ,end=" ")
-    
